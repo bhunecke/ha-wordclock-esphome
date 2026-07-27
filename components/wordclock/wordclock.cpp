25453324b4e35df8d7fd81a0184449874e7996ab
Bastian Hunecke <bastianhunecke@gmail.com>
2026-07-27 19:13:49 +0200

    refactor wordclock time to use proper addressable light effects

--- components/wordclock/wordclock.cpp
+++ components/wordclock/wordclock.cpp
@@ -52,6 +52,10 @@ const char *const log_leds[10][11] = {
 
 void Wordclock::setup() {
   this->led_strip_ = static_cast<light::AddressableLight *>(light_->get_output());
+  this->effect_.set_time(this->time_);
+  this->light_->add_effects({&this->effect_});
+  this->effect_.init_internal(this->light_);
+  this->light_->make_call().set_effect("Wordclock Time").perform();
 }
 
 void Wordclock::on_setled(int number, int red, int blue, int green) {
@@ -64,24 +68,15 @@ void Wordclock::on_setled(int number, int red, int blue, int green) {
   }
 }
 
-void Wordclock::loop() {
-  if (time_ == nullptr || light_ == nullptr || led_strip_ == nullptr) return;
-
-  auto &values = light_->current_values;
-  if (values.get_state() <= 0) return;
+void WordclockEffect::apply(light::AddressableLight &parent, const Color &current_color) {
+  if (time_ == nullptr) return;
 
   auto time = time_->now();
   if (!time.is_valid()) {
-    (*this->led_strip_)[0] = Color(255, 0, 0);
-    this->led_strip_->schedule_show();
+    parent[0] = Color(255, 0, 0, 0);
+    parent.schedule_show();
     return;
   }
-
-  int r = (int) (values.get_red() * values.get_brightness() * 255);
-  int g = (int) (values.get_green() * values.get_brightness() * 255);
-  int b = (int) (values.get_blue() * values.get_brightness() * 255);
-  int w = (int) (values.get_white() * values.get_brightness() * 255);
-  Color color(r, g, b, w);
 
   int h = time.hour;
   int m = time.minute;
@@ -90,37 +85,39 @@ void Wordclock::loop() {
   int tmp_minute = (m - (m % 5)) / 5;
   if ((m % 5) >= 25) tmp_hour = (tmp_hour + 1) % 12;
 
-  int n = this->led_strip_->size();
+  int n = parent.size();
   Color off(0, 0, 0, 0);
   for (int i = 0; i < n; i++) {
-    if (i < 110 || i > 120) (*this->led_strip_)[i] = off;
+    if (i < 110 || i > 120) parent[i] = off;
   }
   for (int i = 0; i < 5; i++) {
-    (*this->led_strip_)[leds_time_it_is[i]] = color;
+    parent[leds_time_it_is[i]] = current_color;
   }
   for (int i = 0; i < 15; i++) {
     if (leds_time_minutes[tmp_minute][i] >= 0) {
-      (*this->led_strip_)[leds_time_minutes[tmp_minute][i]] = color;
+      parent[leds_time_minutes[tmp_minute][i]] = current_color;
     }
   }
   for (int i = 0; i < 6; i++) {
     int led_idx = leds_time_hours[tmp_hour][i];
     if (tmp_hour == 1 && tmp_minute == 0 && led_idx == 60) continue;
-    if (led_idx >= 0) (*this->led_strip_)[led_idx] = color;
+    if (led_idx >= 0) parent[led_idx] = current_color;
   }
 
-  this->led_strip_->schedule_show();
+  parent.schedule_show();
 
   if (m != this->last_log_minute_) {
     this->last_log_minute_ = m;
-    ESP_LOGD(TAG, "Time: %i:%i  RGBW: %i-%i-%i-%i  (tmp_hour: %i tmp_minute: %i)", h, m, r, g, b, w, tmp_hour, tmp_minute);
+    ESP_LOGD(TAG, "Time: %i:%i  RGBW: %i-%i-%i-%i  (tmp_hour: %i tmp_minute: %i)",
+             h, m, current_color.r, current_color.g, current_color.b, current_color.w,
+             tmp_hour, tmp_minute);
     for (int row = 0; row < 10; row++) {
       char line[32];
       int pos = 0;
       int base = row * 11;
       for (int col = 0; col < 11; col++) {
         int idx = (row % 2 == 0) ? base + col : base + (10 - col);
-        auto led = (*this->led_strip_)[idx];
+        auto led = parent[idx];
         const char *s = (led.get_red() + led.get_green() + led.get_blue() + led.get_white() > 0) ? log_leds[row][col] : "-";
         while (*s) line[pos++] = *s++;
       }
--- components/wordclock/wordclock.h
+++ components/wordclock/wordclock.h
@@ -4,14 +4,24 @@
 #include "esphome/components/time/real_time_clock.h"
 #include "esphome/components/light/light_state.h"
 #include "esphome/components/light/addressable_light.h"
+#include "esphome/components/light/addressable_light_effect.h"
 
 namespace esphome {
 namespace wordclock {
 
+class WordclockEffect : public light::AddressableLightEffect {
+ public:
+  WordclockEffect() : AddressableLightEffect("Wordclock Time") {}
+  void set_time(time::RealTimeClock *time) { time_ = time; }
+  void apply(light::AddressableLight &parent, const Color &current_color) override;
+ protected:
+  time::RealTimeClock *time_{nullptr};
+  int last_log_minute_{-1};
+};
+
 class Wordclock : public Component {
  public:
   void setup() override;
-  void loop() override;
 
   void set_time(time::RealTimeClock *time) { time_ = time; }
   void set_light(light::LightState *light) { light_ = light; }
@@ -21,7 +31,7 @@ class Wordclock : public Component {
   time::RealTimeClock *time_{nullptr};
   light::LightState *light_{nullptr};
   light::AddressableLight *led_strip_{nullptr};
-  int last_log_minute_{-1};
+  WordclockEffect effect_;
 };
 
 }  // namespace wordclock
